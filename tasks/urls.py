c3f8f1d9ca99f79f02194ada0f67088c67227d23
Nakamoto-art <bclarence747@gmail.com>
2020-06-22 08:09:34 +0900

    create Form

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='tasks'),
-    path('create_task/', views.createTask, name='create_task'),
+    path('tasks/', views.task_list_view, name='index'),
+    path('tasks/<int:id>/', views.task_detail, name='index'),
+    path('tasks/<int:id>/delete/', views.delete_task),
+    path('create/', views.create_task, name='create_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import TaskForm
+
 from .models import Task
 
 
@@ -6,6 +9,57 @@ def index(request):
     tasks = Task.objects.all()
     return render(request, 'index.html', {'tasks':tasks})
 
-def createTask(request):
-    context = {}
+# def create_task(request):
+#     form = TaskForm()
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Task.objects.create(**form.cleaned_data)
+#         else:
+#             print(form.errors )
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'create_task.html', context)
+
+def create_task(request):
+    form = TaskForm(request.POST or None)
+    if form.is_valid():
+        Task.objects.create(**form.cleaned_data)
+        form = TaskForm()
+        return redirect('../tasks/')
+
+    context = {
+        'form': form
+    }
     return render(request, 'create_task.html', context)
+
+
+def task_detail(request, id):
+    task = get_object_or_404(Task, id=id)
+    context = {
+        'task': task
+    }
+    return render(request, 'task_detail.html', context)
+
+def delete_task(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method =='POST':
+        task.delete()
+        return redirect('../../')
+    context = {
+        "task": task
+    }
+    return render(request, 'delete_task.html', context)
+
+def update_task(request):
+    pass
+
+def task_list_view(request):
+    queryset = Task.objects.all()
+    context = {
+        'task_list': queryset
+    }
+    return render(request, 'task_list.html', context)
